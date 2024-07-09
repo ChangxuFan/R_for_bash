4da3b50bf276f8b84521da6751a20d2b03a6ad71
Changxu Fan <changxu.fan@outlook.com>
2024-07-09 18:56:33 -0500

    update bf.R

--- bf.R
+++ bf.R
@@ -1,1 +1,18 @@
-bash2ftp.R
+#!/bin/bash
+set -e
+# converting bf.R to a bash script to make it faster (avoid loading R every time!)
+# The original bf.R is in bash2ftp.R
+pwd=`pwd`
+for i in $@; do
+	if [[ ! ${i} =~ ^(~|/) ]]; then
+	  # echo "relative path"
+	  i=${pwd}/${i}
+	  # echo $i
+	fi
+	ftp=$(echo "$i" | sed "s|^~|https://wangftp.wustl.edu/~$user|")
+	ftp=$(echo "$ftp" | sed "s|/bar/$user|https://wangftp.wustl.edu/~$user|")
+	ftp=$(echo "$ftp" | sed "s|/scratch/$user/|https://wangftp.wustl.edu/~$user/sth/|")
+	ftp=$(echo "$ftp" | sed "s|/scratch/fanc/|https://wangftp.wustl.edu/~cfan/sth/|")
+
+	echo $ftp
+done
